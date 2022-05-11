f9c6524d08f7f2d3e70b870665188229365a86d7
Ertan Mutlu <ertansofia@gmail.com>
2022-05-11 19:36:21 +0300

    Added my project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./css/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <p>Made by Ertan</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
