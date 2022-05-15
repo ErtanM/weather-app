a16db6d71abf03d374e94c596be8e7e13abf80d0
Ertan Mutlu <ertansofia@gmail.com>
2022-05-15 20:57:15 +0300

    planning v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
 import "./css/App.css";
 import React, { Component } from "react";
+import Weather from "./Weather";
 
 class App extends Component {
   //Gets the data for our api call. We need lat and lon.
-  getLocation = () => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      console.log("Lan:" + position.coords.latitude);
-      console.log("Lon:" + position.coords.longitude);
-    });
-  };
-
   render() {
     return (
       <div className="App">
         <div className="container">
           <h1>Weather App</h1>
           <p>Made by Ertan</p>
-          <button onClick={this.getLocation}>Get Location</button>
+          <Weather />
         </div>
       </div>
     );
---
+++ src/Weather.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+const Weather = () => {
+  const [lat, setLat] = useState(null);
+  const [lng, setLng] = useState(null);
+  const [status, setStatus] = useState(null);
+
+  //Find a way to use lat and lng for api call with await.
+  const getLocation = async () => {
+    if (!navigator.geolocation) {
+      setStatus("Geolocation is not supported by your browser");
+    } else {
+      setStatus("Locating...");
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setStatus(null);
+          setLat(position.coords.latitude);
+          setLng(position.coords.longitude);
+        },
+        () => {
+          setStatus("Unable to retrieve your location");
+        }
+      );
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={getLocation}>Get Location</button>
+      <h1>Coordinates</h1>
+      <p>{status}</p>
+      {lat && <p>Latitude: {lat}</p>}
+      {lng && <p>Longitude: {lng}</p>}
+    </div>
+  );
+};
+
+export default Weather;
