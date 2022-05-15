53e571aa8926fd5bb455c6ef7dd117cfa5a8156d
Ertan Mutlu <ertansofia@gmail.com>
2022-05-15 20:02:33 +0300

    planning

---
+++ src/Weather.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+//API Call here.
+
+//Use useEffect.
+
+// add const options = {
+//     method: 'GET',
+//     url: ...,
+//     headers: {
+
+//     }
+// }
+
+//Use axios?
+
+// const weather_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`;
+// const weather_response = await fetch(weather_url);
+// const weather_data = weather_response.json();
