fa1c18edc3e13f63351c41ea3564303cdc6549fa
Ertan Mutlu <ertansofia@gmail.com>
2022-05-18 18:26:57 +0300

    card design scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 import "./css/App.css";
 import React, { Component } from "react";
 import Weather from "./Weather";
+import axios from "axios";
+import Inf from "./Inf.json";
 
 class App extends Component {
-  //Gets the data for our api call. We need lat and lon.
+  //passing down to json data down
   render() {
     return (
       <div className="App">
-        <div className="container">
-          <h1>Weather App</h1>
-          <p>Made by Ertan</p>
-          <Weather />
+        <div className="outer-card">
+          <div className="container">
+            <div className="title-container">
+              <h1>Weather NOW</h1>
+              <input type="search" name="city-search" id="city-search" />
+            </div>
+            <div className="weather-card">
+              <h2>Sofia</h2>
+              <div className="temp">
+                <p>24.85 °C</p>
+                <p>Clouds</p>
+              </div>
+              <div className="icon">ICON</div>
+              <div className="feels-like">
+                <p>Feels like: 24.85 °C</p>
+                <p>Humidity: 59</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,43 +1,11 @@
 import React, { useState } from "react";
-import axios from "axios";
+
+//receives the props and uses it to display
 
 const Weather = () => {
-  const [lat, setLat] = useState(null);
-  const [lng, setLng] = useState(null);
-  const [status, setStatus] = useState(null);
-
-  //try this with city name!
-  //Add option for current location.
-  //https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-
-  // const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=`;
-
-  //Find a way to use lat and lng for api call with await.
-  const getLocation = async () => {
-    if (!navigator.geolocation) {
-      setStatus("Geolocation is not supported by your browser");
-    } else {
-      setStatus("Locating...");
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          setStatus(null);
-          setLat(position.coords.latitude);
-          setLng(position.coords.longitude);
-        },
-        () => {
-          setStatus("Unable to retrieve your location");
-        }
-      );
-    }
-  };
-
   return (
     <div>
-      <button onClick={getLocation}>Get Location</button>
-      <h1>Coordinates</h1>
-      <p>{status}</p>
-      {lat && <p>Latitude: {lat}</p>}
-      {lng && <p>Longitude: {lng}</p>}
+      <p></p>
     </div>
   );
 };
