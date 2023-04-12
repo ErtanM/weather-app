6a2fbe9b3ede64f24aa98f56aed8309865dff81c
highpriesst <ertansofia@gmail.com>
2023-04-13 00:55:25 +0300

    New functionality added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,57 @@
-import "./css/App.css";
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useState } from "react";
+import useWeather from "./utils/weatherHook";
 
 const App = () => {
-  const [weather, setWeather] = useState("");
-  const [city, setCity] = useState("");
+  const [loc, setLoc] = useState("");
+  const [weather, city, apiCall] = useWeather();
 
-  const apiCall = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const loc = e.target.elements.loc.value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${
-      import.meta.env.VITE_API_KEY
-    }`;
-    const req = axios.get(url);
-    const res = await req;
-    setWeather({
-      descp: res.data.weather[0].description,
-      temp: res.data.main.temp,
-      city: res.data.name,
-      humidity: res.data.main.humidity,
-      press: res.data.main.pressure,
-    });
-
-    setCity(res.data.name);
+    apiCall(e);
+    setLoc("");
   };
 
   return (
-    <div className="App">
-      <div className="bottom">
-        <div className="input">
-          <form onSubmit={apiCall} className="form">
-            <input type="text" placeholder="city" name="loc" />
-            <button className="bttn">Search</button>
-          </form>
-        </div>
-        <div className="title">
-          <h2>{city}</h2>
-        </div>
-        <div className="temp">
-          <p>Temp: {weather.temp}</p>
-          <p></p>
-        </div>
-        <div className="icon"></div>
-
-        <div className="feels-like">
-          <p className="first">Feels like: {weather.temp}</p>
-          <p className="second">
-            Humidity: {weather.humidity}
-            <br />
-          </p>
-        </div>
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <div className="bg-white rounded-lg shadow-lg p-8 max-w-sm w-full space-y-4">
+        <form onSubmit={handleSubmit}>
+          <div className="flex items-center">
+            <input
+              type="text"
+              placeholder="City"
+              value={loc}
+              onChange={(e) => setLoc(e.target.value)}
+              name="loc"
+              className="w-full py-2 px-4 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            />
+            <button className="ml-2 px-4 py-2 bg-blue-500 text-white rounded-lg shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-900">
+              Search
+            </button>
+          </div>
+        </form>
+        {city && (
+          <div className="flex flex-col items-center space-y-2">
+            <h2 className="text-xl font-medium">{city}</h2>
+            <div className="flex items-center space-x-4">
+              <p className="text-gray-600 text-lg">Temperature:</p>
+              <p className="text-4xl font-medium">{Math.round(weather.temp)}</p>
+            </div>
+            <div className="flex items-center space-x-4">
+              <p className="text-gray-600 text-lg">Feels like:</p>
+              <p className="text-lg font-medium">
+                {Math.round(weather.feels_like)}
+              </p>
+            </div>
+            <div className="flex items-center space-x-4">
+              <p className="text-gray-600 text-lg">Humidity:</p>
+              <p className="text-lg font-medium">{weather.humidity}</p>
+            </div>
+            <div className="flex items-center space-x-4">
+              <p className="text-gray-600 text-lg">Description:</p>
+              <p className="text-lg font-medium">{weather.descp}</p>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
---
+++ src/utils/weatherHook.js
@@ -0,0 +1,33 @@
+//Create custom hook here to fetch data.
+import { useState, useCallback } from "react";
+import axios from "axios";
+
+const useWeather = () => {
+  const [weather, setWeather] = useState("");
+  const [city, setCity] = useState("");
+
+  const apiCall = useCallback(async (e) => {
+    e.preventDefault();
+    const loc = e.target.elements.loc.value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${
+      import.meta.env.VITE_API_KEY
+    }&units=metric`;
+    const req = axios.get(url);
+    const res = await req;
+    setWeather({
+      descp: res.data.weather[0].main,
+      temp: res.data.main.temp,
+      city: res.data.name,
+      feels_like: res.data.main.feels_like,
+      humidity: res.data.main.humidity,
+      press: res.data.main.pressure,
+    });
+    setCity(res.data.name);
+
+    console.log(res.data);
+  }, []);
+
+  return [weather, city, apiCall];
+};
+
+export default useWeather;
