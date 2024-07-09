f57c8d3e72b83bf5ae2834f2e091fa14b876482d
highpriesst <ertansofia@gmail.com>
2024-07-09 23:02:43 +0200

    Refactoring the code for more readability and functionality.

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-const BASE_API = import.meta.env.VITE_API_KEY;
 
 //Type for geolocation's info. Altho it's Number initially, fixed to String to use it inside fetch.
 interface Location {
@@ -17,32 +16,42 @@ const useWeather = (apiKey: string) => {
 
   //Getting user's location coordinates before fetching.
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
+    const handleSuccess = (position: GeolocationPosition) => {
       setLocation({
-        lat: position.coords.latitude.toFixed(4).toString(),
-        lon: position.coords.longitude.toFixed(4).toString(),
+        lat: position.coords.latitude.toFixed(4),
+        lon: position.coords.longitude.toFixed(4),
       });
-    });
+    };
+
+    const handleError = (error: GeolocationPositionError) => {
+      setError(error.message);
+    };
+
+    navigator.geolocation.getCurrentPosition(handleSuccess, handleError);
   }, []);
 
-  //Checking if the location data is successfully stored and fetching the weather api's data depending on the location.
+  //Checking if the location and apiKey data is successfully stored and fetching the weather api's data depending on the location.
   useEffect(() => {
+    const fetchData = async (lat: string, lon: string) => {
+      try {
+        const response = await fetch(
+          `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${lat},${lon}&days=3`
+        );
+        const result = await response.json();
+        setWeatherData(result);
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          setError(error.message);
+        } else {
+          setError("An unknown error occurred");
+        }
+      }
+    };
+
     if (location.lat && location.lon) {
       fetchData(location.lat, location.lon);
     }
-
-    // console.log(weatherData);
-  }, [location]);
-
-  //Fetching function to be used inside useEffect.
-  const fetchData = async (lat: string, lon: string) => {
-    await fetch(
-      `http://api.weatherapi.com/v1/forecast.json?key=${BASE_API}&q=${lat},${lon}&days=3`
-    )
-      .then((res) => res.json())
-      .then((result) => setWeatherData(result))
-      .catch((err) => setError(err));
-  };
+  }, [location, apiKey]);
 
   return { weatherData, error };
 };
